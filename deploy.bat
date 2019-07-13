98490281e7c0e3e65f2ad91ad7a3a4bb7e4891ac
maragnik <maragnik@gmail.com>
2019-07-13 19:22:43 +0300

    Initial Commit

---
+++ deploy.bat
@@ -0,0 +1,4 @@
+set QT_INSTALLATION_BIN_DIR = ":\Tools\Qt\5.13.0\msvc2017_64\bin"
+set APP_BINARY_FOLDER = "C:\prj\elkethe\projects\HCMR_Minimization\_build\Release"
+
+%QT_INSTALLATION_BIN_DIR%\windeployqt %APP_BINARY_FOLDER%
