e1d0fcbab83b05e7da8d7f15d1e41770c46dfdd1
maragnik <maragnik@gmail.com>
2019-07-14 11:26:35 +0300

    Restructure and add test

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,8 +1,10 @@
 ﻿#include <QApplication>
 #include "mainwindow.h"
+#include "HCMRUnitaryInput.h"
 
 int main(int argc, char* argv[]) {
 	QApplication a(argc, argv);
+	HCMRUnitaryInput<int, int> input;
 	MainWindow w;
 	w.show();
 	return a.exec();
---
+++ SubProjects/test/MyTest.cpp
@@ -0,0 +1,7 @@
+﻿#include "../HCMRMinimizationLib/HCMRUnitaryInput.h"
+
+int main(int argc, char* argv[]) {
+	HCMRUnitaryInput<int, int> input;
+	input.fillFromFile("C:\\prj\\elkethe\\HCMR_Minimization_App\\Extra\\test.csv");
+	return 0;
+}
